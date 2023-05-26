3755a51aa4ced96bf6fd1707016447e18dab5204
Lee-Git22 <lee.alan9801@gmail.com>
2023-05-26 14:53:08 -0400

    Implement depth first search for knight moves

--- main.rb
+++ main.rb
@@ -1,39 +1,39 @@
 
 class Knight
-  attr_accessor :position, :possible_moves
+  attr_accessor :position, :parent, :move_history
 
-  def initialize(position=[], possible_moves=[] )
+  MOVEMENT_PATTERN = [ [1,2], [-1,2], [1,-2], [-1, -2], [2,1], [-2,1], [2, -1], [-2, -1] ]
+
+  def initialize(position=[], parent=nil, move_history=[])
     @position = position
-    @possible_moves = possible_moves
+    @parent = parent
+    @move_history = [position]
   end
 
   def calcMoves(current_position=position)
-    newMoves = []
+    
 
-    newMoves << [ current_position[0]+2, current_position[1]+1 ] 
-    newMoves << [ current_position[0]-2, current_position[1]+1 ]
-    newMoves << [ current_position[0]+2, current_position[1]-1 ]
-    newMoves << [ current_position[0]-2, current_position[1]-1 ]
-    newMoves << [ current_position[0]+1, current_position[1]+2 ]
-    newMoves << [ current_position[0]-1, current_position[1]+2 ]
-    newMoves << [ current_position[0]+1, current_position[1]-2 ]
-    newMoves << [ current_position[0]-1, current_position[1]-2 ]
-
+    MOVEMENT_PATTERN.map { |pattern| [@position[0] + pattern[0], @position[1] + pattern[1]] }
+                              .keep_if {|move| Knight.valid?(move)}
+                              .reject { |move| @move_history.include?(move)}
+                              .map { |move| Knight.new(move, self)}
+    
   end
 
-  def validateMoves(newMoves)
-    newMoves.select! {|newMove| 
-    newMove[0] <= 7 and newMove[0] >= 0 and 
-    newMove[1] <= 7 and newMove[1] >= 0}
+  def self.valid?(position)
+    position[0].between?(0, 7) && position[1].between?(0, 7)
   end
+
 end
 
 class Board
-  attr_accessor :board, :knight
+  attr_accessor :board, :start, :target, :path
 
   def initialize()
     @board = spawnBoard()
-    @knight = Knight.new(position=spawnKnight())
+    @start = [rand(0..7), rand(0..7)]  
+    @target = [rand(0..7), rand(0..7)]
+    @path = []
   end
 
   # Spawns an empty 8x8 array with 0s
@@ -47,18 +47,11 @@ class Board
   end
 
 
-  # Spawns a knight on random coordinate on chess board
-  def spawnKnight()
-    position = Array.new(2)
-    position[0] = rand(0..7)
-    position[1] = rand(0..7)
-    puts toChessCoordinates(position)
-    position
-  end
-
   # Draws board and labels coordinates conventionally as per chess rules
   def drawBoard()
-    board[self.knight.position[1]][self.knight.position[0]] = 1
+    pathCount = 1
+    path.each { |position| board[position[1]][position[0]] = pathCount; pathCount += 1}
+
     8.times do |i|
       puts "#{i+1} #{self.board[i]}"
     end
@@ -69,19 +62,35 @@ class Board
   def toChessCoordinates(position)
     row_ref = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
     coordinate = ''
-
+    
     coordinate << row_ref[position[0]]
     coordinate << (position[1] + 1).to_s
   end
+
+  def display_move_history(node)
+    display_move_history(node.parent) unless node.parent.nil?
+    @path << node.position 
+    puts toChessCoordinates(node.position)
+  end
+
+  def knight_moves(start_pos=@start, end_position=@target)
+    queue = []
+    current_node = Knight.new(start_pos)
+    until current_node.position == end_position
+      current_node.calcMoves.each { |child| queue.push(child) }
+      current_node = queue.shift
+    end
+    
+    display_move_history(current_node)
+    drawBoard()
+
+  end
+
 end
 
-board = Board.new()
 
-board.drawBoard
 
-test = board.knight.calcMoves
-new_moves_test = board.knight.validateMoves(test)
 
-for moves in new_moves_test
-  puts board.toChessCoordinates(moves)
-end
+quickest_move = Board.new()
+
+quickest_move.knight_moves()
